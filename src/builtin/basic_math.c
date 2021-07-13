417a93897dc0467b20fe55e0b849fcaa523681b5
Abb1x <abb1x@protonmail.com>
2021-07-13 12:03:17 -0400

    Feat: add * / and argument count checking

--- src/builtin/basic_math.c
+++ src/builtin/basic_math.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <types.h>
 #include <stdlib.h>
+#include <types.h>
 #include <vec/vec.h>
 
 scm_var_t scm_add(scm_var_t args)
@@ -11,12 +11,18 @@ scm_var_t scm_add(scm_var_t args)
 
     int i;
     scm_var_t number;
+    
+    if (args._toks.length <= 0)
+    {
+        printf("ArgumentError: missing arguments");
+        return scm_token(SCM_NIL, NULL);
+    }
 
     vec_foreach(&args._toks, number, i)
     {
         if (number.type != SCM_NUMBER_FLOAT && number.type != SCM_NUMBER_INT)
         {
-            fprintf(stderr, "TypeError: Can only add Numbers\n");
+            fprintf(stderr, "TypeError: Can only add numbers\n");
             return scm_token(SCM_NIL, NULL);
         }
 
@@ -26,19 +32,18 @@ scm_var_t scm_add(scm_var_t args)
             {
                 size_t nbr = ret._nbr;
                 ret._nbr = 0;
-                ret._float += (double) nbr;
+                ret._float += (double)nbr;
                 ret.type = SCM_NUMBER_FLOAT;
             }
 
-           
-            ret._float += (double) number._float;
+            ret._float += (double)number._float;
         }
-        else if(number.type == SCM_NUMBER_INT && ret.type == SCM_NUMBER_FLOAT)
+        else if (number.type == SCM_NUMBER_INT && ret.type == SCM_NUMBER_FLOAT)
         {
-            ret._float += (double) number._nbr;
+            ret._float += (double)number._nbr;
         }
 
-        else 
+        else
         {
             ret._nbr += number._nbr;
         }
@@ -47,25 +52,32 @@ scm_var_t scm_add(scm_var_t args)
     return ret;
 }
 
-
-scm_var_t scm_subs(scm_var_t args)
+scm_var_t scm_sub(scm_var_t args)
 {
     scm_var_t ret;
     ret.type = SCM_NUMBER_INT;
-
+    
+    if (args._toks.length <= 0)
+    {
+        printf("ArgumentError: missing arguments");
+        return scm_token(SCM_NIL, NULL);
+    }
+    
     if (args._toks.data[0].type != SCM_NUMBER_FLOAT && args._toks.data[0].type != SCM_NUMBER_INT)
     {
-        fprintf(stderr, "TypeError: Can only add Numbers\n");
+        fprintf(stderr, "TypeError: Can only subtract numbers\n");
         return scm_token(SCM_NIL, NULL);
     }
 
-    if (args._toks.length == 1)
+
+
+    else if (args._toks.length == 1)
     {
         if (args._toks.data[0].type == SCM_NUMBER_INT)
         {
             ret._nbr = 0 - args._toks.data[0]._nbr;
         }
-        else 
+        else
         {
             ret.type = SCM_NUMBER_FLOAT;
             ret._float = 0 - args._toks.data[0]._float;
@@ -73,25 +85,24 @@ scm_var_t scm_subs(scm_var_t args)
 
         return ret;
     }
-    else 
+    else
     {
         if (args._toks.data[0].type == SCM_NUMBER_INT)
         {
             ret._nbr = args._toks.data[0]._nbr;
         }
-        else 
+        else
         {
             ret.type = SCM_NUMBER_FLOAT;
             ret._float = 0 - args._toks.data[0]._float;
         }
     }
 
-
     for (int i = 1; i < args._toks.length; i++)
     {
         if (args._toks.data[i].type != SCM_NUMBER_FLOAT && args._toks.data[i].type != SCM_NUMBER_INT)
         {
-            fprintf(stderr, "TypeError: Can only add Numbers\n");
+            fprintf(stderr, "TypeError: Can only subtract numbers\n");
             return scm_token(SCM_NIL, NULL);
         }
 
@@ -101,19 +112,177 @@ scm_var_t scm_subs(scm_var_t args)
             {
                 size_t nbr = ret._nbr;
                 ret._nbr = 0;
-                ret._float = (double) nbr;
+                ret._float = (double)nbr;
                 ret.type = SCM_NUMBER_FLOAT;
             }
 
-            ret._float -= (double) args._toks.data[i]._float;
+            ret._float -= (double)args._toks.data[i]._float;
         }
-        else if(args._toks.data[i].type == SCM_NUMBER_INT && ret.type == SCM_NUMBER_FLOAT)
+        else if (args._toks.data[i].type == SCM_NUMBER_INT && ret.type == SCM_NUMBER_FLOAT)
         {
-            ret._float -= (double) args._toks.data[i]._nbr;
+            ret._float -= (double)args._toks.data[i]._nbr;
         }
-        else 
+        else
         {
             ret._nbr -= args._toks.data[i]._nbr;
+        }
+    }
+
+    return ret;
+}
+
+scm_var_t scm_div(scm_var_t args)
+{
+    scm_var_t ret;
+    ret.type = SCM_NUMBER_INT;
+    
+    if (args._toks.length <= 0)
+    {
+        printf("ArgumentError: missing arguments");
+	
+        return scm_token(SCM_NIL, NULL);
+    }
+    
+    if (args._toks.data[0].type != SCM_NUMBER_FLOAT && args._toks.data[0].type != SCM_NUMBER_INT)
+    {
+        fprintf(stderr, "TypeError: Can only divide numbers\n");
+        return scm_token(SCM_NIL, NULL);
+    }
+
+
+    else if (args._toks.length == 1)
+    {
+        if (args._toks.data[0].type == SCM_NUMBER_INT)
+        {
+            ret._nbr = 0 - args._toks.data[0]._nbr;
+        }
+        else
+        {
+            ret.type = SCM_NUMBER_FLOAT;
+            ret._float = 0 - args._toks.data[0]._float;
+        }
+
+        return ret;
+    }
+    else
+    {
+        if (args._toks.data[0].type == SCM_NUMBER_INT)
+        {
+            ret._nbr = args._toks.data[0]._nbr;
+        }
+        else
+        {
+            ret.type = SCM_NUMBER_FLOAT;
+            ret._float = 0 - args._toks.data[0]._float;
+        }
+    }
+
+    for (int i = 1; i < args._toks.length; i++)
+    {
+        if (args._toks.data[i].type != SCM_NUMBER_FLOAT && args._toks.data[i].type != SCM_NUMBER_INT)
+        {
+            fprintf(stderr, "TypeError: Can only divide numbers\n");
+            return scm_token(SCM_NIL, NULL);
+        }
+
+        if (args._toks.data[i].type == SCM_NUMBER_FLOAT)
+        {
+            if (ret.type == SCM_NUMBER_INT)
+            {
+                size_t nbr = ret._nbr;
+                ret._nbr = 0;
+                ret._float = (double)nbr;
+                ret.type = SCM_NUMBER_FLOAT;
+            }
+
+            ret._float /= (double)args._toks.data[i]._float;
+        }
+        else if (args._toks.data[i].type == SCM_NUMBER_INT && ret.type == SCM_NUMBER_FLOAT)
+        {
+            ret._float /= (double)args._toks.data[i]._nbr;
+        }
+        else
+        {
+            ret._nbr /= args._toks.data[i]._nbr;
+        }
+    }
+
+    return ret;
+}
+
+scm_var_t scm_mul(scm_var_t args)
+{
+    scm_var_t ret;
+    ret.type = SCM_NUMBER_INT;
+
+    if (args._toks.length <= 0)
+    {
+        printf("ArgumentError: missing arguments");
+	
+        return scm_token(SCM_NIL, NULL);
+    }
+    
+    if (args._toks.data[0].type != SCM_NUMBER_FLOAT && args._toks.data[0].type != SCM_NUMBER_INT)
+    {
+        fprintf(stderr, "TypeError: Can only multiply numbers\n");
+        return scm_token(SCM_NIL, NULL);
+    }
+
+
+    else if (args._toks.length == 1)
+    {
+        if (args._toks.data[0].type == SCM_NUMBER_INT)
+        {
+            ret._nbr = 0 - args._toks.data[0]._nbr;
+        }
+        else
+        {
+            ret.type = SCM_NUMBER_FLOAT;
+            ret._float = 0 - args._toks.data[0]._float;
+        }
+
+        return ret;
+    }
+    else
+    {
+        if (args._toks.data[0].type == SCM_NUMBER_INT)
+        {
+            ret._nbr = args._toks.data[0]._nbr;
+        }
+        else
+        {
+            ret.type = SCM_NUMBER_FLOAT;
+            ret._float = 0 - args._toks.data[0]._float;
+        }
+    }
+
+    for (int i = 1; i < args._toks.length; i++)
+    {
+        if (args._toks.data[i].type != SCM_NUMBER_FLOAT && args._toks.data[i].type != SCM_NUMBER_INT)
+        {
+            fprintf(stderr, "TypeError: Can only multiply numbers\n");
+            return scm_token(SCM_NIL, NULL);
+        }
+
+        if (args._toks.data[i].type == SCM_NUMBER_FLOAT)
+        {
+            if (ret.type == SCM_NUMBER_INT)
+            {
+                size_t nbr = ret._nbr;
+                ret._nbr = 0;
+                ret._float = (double)nbr;
+                ret.type = SCM_NUMBER_FLOAT;
+            }
+
+            ret._float *= (double)args._toks.data[i]._float;
+        }
+        else if (args._toks.data[i].type == SCM_NUMBER_INT && ret.type == SCM_NUMBER_FLOAT)
+        {
+            ret._float *= (double)args._toks.data[i]._nbr;
+        }
+        else
+        {
+            ret._nbr *= args._toks.data[i]._nbr;
         }
     }
 
